a1bef10ed009a4b00c85b0224811a06b62e87a41
Will Koste <willkoste@gmail.com>
2020-05-08 11:23:42 -0400

    Client Folder of File Uploader

---
+++ src/components/Progress.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Progress = ({percentage}) => {
+  return (
+    <div>
+      <div className="progress">
+        <div className="progress-bar progress-bar-striped bg-danger" role="progressbar" style={{width: `${percentage}%`}}></div>
+      </div>
+      <small>{percentage ? `${percentage}%` : ''}</small>
+    </div>
+  )
+}
+
+Progress.propTypes = {
+  percentage: PropTypes.number.isRequired
+}
+
+export default Progress;
